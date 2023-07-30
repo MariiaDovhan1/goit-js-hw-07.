a24c931ac340cff448cc5ac36b10f97f1b4deb35
MariiaDovhan1 <125833245+MariiaDovhan1@users.noreply.github.com>
2023-07-30 10:38:08 +0200

    jjj

---
+++ js/02-lightbox.js
@@ -0,0 +1,28 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryList = document.querySelector('ul.gallery');
+
+const createCardsMarkup = cards => {
+    const { preview, original, description } = cards;
+    return `
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          title="${description}"
+        />
+      </a>
+  `;
+};
+
+const createCardsGallery = [...galleryItems].map(createCardsMarkup).join('');
+galleryList.insertAdjacentHTML('beforeend', createCardsGallery);
+
+new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    fadeSpeed: 150,
+    animationSlide: false,
+});
+console.log(galleryItems);
